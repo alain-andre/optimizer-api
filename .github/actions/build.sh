5e83a1c5d8c62480904c66169dae61f6b65a3472
Alain ANDRE <wordsbybird@alain-andre.fr>
2021-04-07 17:24:55 +0200

    tmp

--- .github/actions/build.sh
+++ .github/actions/build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --build-arg BUNDLE_WITHOUT=development --build-arg VROOM_VERSION=${VROOM_VERSION} --build-arg OPTIMIZER_ORTOOLS_VERSION=${OPTIMIZER_ORTOOLS_VERSION} -f docker/Dockerfile -t registry.test.com/mapotempo/optimizer-api:latest .
-
-# Cache generated gems
-mkdir -p vendor/bundle
-docker cp $(docker create --rm registry.test.com/mapotempo/optimizer-api:latest):/srv/app/vendor/bundle/. vendor/bundle
+docker build --build-arg BUNDLE_WITHOUT=development --build-arg VROOM_VERSION=v1.5.0 --build-arg OPTIMIZER_ORTOOLS_VERSION=${OPTIMIZER_ORTOOLS_VERSION} -f docker/Dockerfile -t registry.test.com/mapotempo/optimizer-api:latest .
